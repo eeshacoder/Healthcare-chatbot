8557260a5e4c5a6cbd9189ac9beb2d8d7e798677
eeshacoder <154540767+eeshacoder@users.noreply.github.com>
2024-01-19 21:17:53 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier,_tree
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import pyttsx3
+engine = pyttsx3.init()
+
+import pandas as pd
+training = pd.read_csv('Training.csv')
+testing= pd.read_csv('Testing.csv')
+cols= training.columns
+cols= cols[:-1]
+x = training[cols]
+y = training['prognosis']
+y1= y
+
+
+reduced_data = training.groupby(training['prognosis']).max()
+
+#mapping strings to numbers
+le = preprocessing.LabelEncoder()
+le.fit(y)
+y = le.transform(y)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+testx    = testing[cols]
+testy    = testing['prognosis']  
+testy    = le.transform(testy)
+
+
+clf1  = DecisionTreeClassifier()
+clf = clf1.fit(x_train,y_train)
+# print(clf.score(x_train,y_train))
+# print ("cross result========")
+scores = cross_val_score(clf, x_test, y_test, cv=3)
+# print (scores)
+print("for DecisionTreeClassifier: ")
+print (scores.mean())
+
+
+model=SVC()
+model.fit(x_train,y_train)
+print("for svm: ")
+print(model.score(x_test,y_test))
+
+
+filenumber=int(os.listdir('saved_conversations')[-1])
+filenumber=filenumber+1
+file= open('saved_conversations/'+str(filenumber),"w+")
+file.write('bot : Hi There! I am a medical chatbot. You can begin conversation by typing in a message and pressing enter.\n')
+file.close()
+
+app = Flask(__name__)
+
+
+english_bot = ChatBot('Bot',
+             storage_adapter='chatterbot.storage.SQLStorageAdapter',
+             logic_adapters=[
+   {
+       'import_path': 'chatterbot.logic.BestMatch'
+   },
+   
+],
+trainer='chatterbot.trainers.ListTrainer')
+english_bot.set_trainer(ListTrainer)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = str(english_bot.get_response(userText))
+
+    appendfile=os.listdir('saved_conversations')[-1]
+    appendfile= open('saved_conversations/'+str(filenumber),"a")
+    appendfile.write('user : '+userText+'\n')
+    appendfile.write('bot : '+response+'\n')
+    engine.say('bot : '+response+'\n')
+    engine.runAndWait()
+    appendfile.close()
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
